31c784b25522ad90cbe234436de9041a619b8258
thai19052001 <dinhthai1905@gmail.com>
2023-11-21 13:43:37 +0700

    Remake repo

---
+++ popup.js
@@ -0,0 +1,6 @@
+// const btn = document.getElementById('button');
+// var changebtn = function(){
+//     const btn = document.getElementById('button');
+//     btn.classList.remove('btn-1');
+//     btn.classList.add('btn-2');
+// }
