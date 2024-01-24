346c46fdeb90f7b0580c8e284bd642780a820396
thai19052001 <dinhthai1905@gmail.com>
2024-01-24 10:24:47 +0700

    SCSS

--- popup.js
+++
@@ -1,6 +0,0 @@
-// const btn = document.getElementById('button');
-// var changebtn = function(){
-//     const btn = document.getElementById('button');
-//     btn.classList.remove('btn-1');
-//     btn.classList.add('btn-2');
-// }
